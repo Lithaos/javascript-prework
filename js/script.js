79b1a4ae9bc15b7e4b3a32f7fefe97bd5ab26bdf
Wojciech Raczynski <wojto3@op.pl>
2019-03-18 10:03:18 +0100

    Add user answer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
 } else if (randomNumber == '2') {
@@ -9,3 +9,17 @@ if (randomNumber == '1') {
   computerMove = 'nożyce';
 }
 console.log('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+console.log('Twój ruch: ' + playerMove);
