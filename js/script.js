ed013c956388cad77cc0a423e944022e894da5a9
Wojciech Raczynski <wojto3@op.pl>
2019-03-18 10:57:29 +0100

    add game rules

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else if (randomNumber == '2') {
-  computerMove = 'papier';
-} else if (randomNumber == '3') {
-  computerMove = 'nożyce';
-}
-console.log('Mój ruch: ' + computerMove);
+var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
 
-var playerMove, playerInput;
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove = 'papier';
-} else if (playerInput == '3') {
-  playerMove = 'nożyce';
-} else {
-  playerMove = 'nieznany ruch';
+/**
+ * Describe this function...
+ */
+function getMoveName(argMoveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nożyce';
+  } else {
+    console.log('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+    return 'kamień';
+  }
 }
-console.log('Twój ruch: ' + playerMove);
+
+/**
+ * Describe this function...
+ */
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+    console.log('Wygrywasz!');
+  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+    console.log('Wygrywasz!');
+  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+    console.log('Wygrywasz!');
+  } else if (argPlayerMove == argComputerMove && true) {
+    console.log('Remis');
+  } else {
+    console.log('Przegrywasz :(');
+  }
+  console.log('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+}
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('wybór ruchu gracza to: ' + playerInput);
+playerMove = getMoveName(playerInput);
+console.log('ruch gracza to: ' + playerMove);
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log('ruch komputera to: ' + computerMove);
+displayResult(playerMove, computerMove);
